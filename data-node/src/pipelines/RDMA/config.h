61f654ef7b379a002b718c2432ca93fc998266dd
Felix Zhang <felixfzhang@hotmail.com>
2026-04-12 17:09:31 -0400

    update RDMA addr

--- data-node/src/pipelines/RDMA/config.h
+++ data-node/src/pipelines/RDMA/config.h
@@ -4,10 +4,5 @@
 
 #pragma once
 
-#ifdef DOCA_ARCH_DPU
-constexpr char PCIE_ADDRESS[] = "0000:03:00.1";
-constexpr uint8_t GID = 1;
-#else
-constexpr char PCIE_ADDRESS[] = "0000:21:00.1";
+constexpr char PCIE_ADDRESS[] = "0000:5e:00.1";
 constexpr uint8_t GID = 3;
-#endif
